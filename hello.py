c3b6235c9a3762f32a2366f7bf7ec4d8c041d153
mperkins20 <mperkins20@pvamu.edu>
2025-03-11 08:06:50 -0500

    Fixed input validation: Prevents empty names.

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello from both main and feature-2!")
+name = input("Enter your name: ").strip()
+while not name:
+    name = input("Name cannot be empty! Enter your name: ").strip()
+
+print(f"Hello, {name}!")
